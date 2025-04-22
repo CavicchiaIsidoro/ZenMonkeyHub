0d69b5230eb4f64779213756500dc5898953a486
MASTER\t15f56 <isidoro.cavicchia@solidaris.be>
2025-04-22 19:32:29 +0200

    #000000000

--- ZenMonkey.Hub/ZenMonkey.Hub.Api/Program.cs
+++ ZenMonkey.Hub/ZenMonkey.Hub.Api/Program.cs
@@ -1,6 +1,29 @@
+using ZenMonkey.Application.Telegram.Services;
+using ZenMonkey.Application.Telegram.Services.Interfaces;
+using ZenMonkey.Application.Trades.Services;
+using ZenMonkey.Application.Trades.Services.Interfaces;
+using ZenMonkey.Hub.Infrastructure.GeckoTerminal.Interface;
+using ZenMonkey.Hub.Infrastructure.GeckoTerminal.Repository;
+using ZenMonkey.Hub.Infrastructure.Logger;
+using ZenMonkey.Hub.Infrastructure.Telegram.Interfaces;
+using ZenMonkey.Hub.Infrastructure.Telegram.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(10);
+});
+
 // Add services to the container.
+builder.Services.AddHostedService<BotService>();
+
+builder.Services.AddScoped<ITradeService, TradeService>();
+builder.Services.AddScoped<ITelegramService, TelegramService>();
+builder.Services.AddScoped<ITradeRepository, TradeRepository>();
+builder.Services.AddScoped<ITelegramRepository, TelegramRepository>();
+
+builder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,12 +32,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
--- ZenMonkey.Hub/ZenMonkey.Hub.Api/obj/Debug/net8.0/ZenMonkey.Hub.Api.AssemblyInfo.cs
+++ ZenMonkey.Hub/ZenMonkey.Hub.Api/obj/Debug/net8.0/ZenMonkey.Hub.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZenMonkey.Hub.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e04b2d523b68d7f276b135510ed0bc48a5bd303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50907da77cd72835d48e7feebc2a293770b81b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZenMonkey.Hub.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZenMonkey.Hub.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
